ef6d17cfd4ce547fa6e3266a830284156d0991cf
Velo <hartley.max@outlook.com>
2021-10-28 14:53:32 +0100

    create basic html preview

--- src/components/HTMLPreview/index.js
+++ src/components/HTMLPreview/index.js
@@ -1,7 +1,13 @@
-export default function index() {
+import MarkdownIt from 'markdown-it';
+import parse from 'html-react-parser';
+
+export default function index({ markdown }) {
+	const md = new MarkdownIt();
+	const html = md.render(markdown);
 	return (
 		<div>
 			<h1>HTML Preview</h1>
+			<section>{parse(html)}</section>
 		</div>
 	);
 }
