bd12310bcd47616842dbfec399ae10113cae3565
Velo <hartley.max@outlook.com>
2021-10-28 14:39:49 +0100

    create markdown input

--- src/components/MarkdownInput/index.js
+++ src/components/MarkdownInput/index.js
@@ -1,7 +1,15 @@
-export default function index() {
+export default function index({ markdown, updateMarkdown }) {
 	return (
 		<div>
 			<h1>Markdown Input</h1>
+			<textarea
+				value={markdown}
+				onChange={updateMarkdown}
+				name='Markdown input'
+				id=''
+				cols='30'
+				rows='10'
+			></textarea>
 		</div>
 	);
 }
--- src/pages/MarkdownEditor/index.js
+++ src/pages/MarkdownEditor/index.js
@@ -1,11 +1,18 @@
+import { useState } from 'react';
 import { HTMLPreview, MarkdownInput } from '../../components';
 import styles from './styles.module.css';
 
-export default function index() {
+export default function Index() {
+	const [markdown, setMarkdown] = useState('');
+
+	const updateMarkdown = (e) => {
+		setMarkdown(e.target.value);
+	};
+
 	return (
 		<main className={styles.main}>
-			<MarkdownInput />
-			<HTMLPreview />
+			<MarkdownInput markdown={markdown} updateMarkdown={updateMarkdown} />
+			<HTMLPreview markdown={markdown} />
 		</main>
 	);
 }
